6540cf0a98eb69bac2acc8b08b3f180dc5bc8512
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-09 12:41:02 -0300

    feat: add token interceptation in api instance request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 import axios from 'axios';
 
 const api = axios.create({
@@ -5,6 +6,13 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = localStorage.getItem('@Pomodone:token');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+
+  return config;
 });
 
 export default api;
